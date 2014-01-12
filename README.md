a0838ba119b75873177ef9ca497e5c7e269b406d
Sallar Kaboli <sallar.kaboli@gmail.com>
2014-01-12 18:38:06 +0330

    First Commit
    Added all website files.

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-rwd-conf-92
+RWD Conference 1392
 ===========
 
-Website for 1st Responsive Design Conference in Iran.
+Website for 1st Responsive Design Conference in Iran.  
+Designed and Developed by [Sallar Kaboli](http://sallar.me) for [Web Standards School](http://wsschool.org)’s RWD Conference which was held Dec 5th 2012 in Tehran, Iran.
+
+##Notes
+The CSS has been compiled using [Sass](http://sass-lang.com/), if you need to modify the CSS, please modify the Sass source located at `static/lib/style.scss` and compile using a Sass compiler or a build tool like [Grunt](http://gruntjs.com/). DO NOT modify the CSS directly as it will be overwritten by the compiler.
+
+##Logos License
+There are some logos present in the repository which are trademarks of their respective owners. You **ARE NOT** allowed to use their logos and trademarks in ANY project without permission.
+
+
+##License
+This program is licensed under [GPL v2](http://choosealicense.com/licenses/gpl-v2/) unless its otherwise noted. A full version of the license has been added to the repository for your reference.
+
+    (C) 2013 - Sallar Kaboli <sallar.kaboli@gmail.com>
+    Web Standards School <wsschool.org>.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
---
+++ static/js/app.js
@@ -0,0 +1,78 @@
+/*------------------------------------------------------*\
+
+     _ _ _ _____ _____ _____ _____ _____ _____ __      _____ _____ _____ 
+    | | | |   __|   __|     |  |  |     |     |  |    |     | __  |   __|
+    | | | |__   |__   |   --|     |  |  |  |  |  |__ _|  |  |    -|  |  |
+    |_____|_____|_____|_____|__|__|_____|_____|_____|_|_____|__|__|_____|
+
+    (C) 2013 - Sallar Kaboli <sallar.kaboli@gmail.com>
+    Web Standards School <wsschool.org>.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+\*------------------------------------------------------*/
+
+var ie = (function(){
+
+    var undef,
+        v = 3,
+        div = document.createElement('div'),
+        all = div.getElementsByTagName('i');
+
+    while (
+        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+        all[0]
+    );
+
+    return v > 4 ? v : undef;
+
+}());
+
+/**
+ * Add IE Class
+ */
+if( ie && ie < 9 ) {
+    $('body').addClass('ie');
+}
+
+/**
+ * Add Map
+ */
+if( $('.map-container').length > 0 )
+{   
+    var $map = $('.map-container'),
+
+    mapOptions = {
+        zoom               : 8,
+        center             : new google.maps.LatLng(35.732766,51.389306),
+        mapTypeId          : google.maps.MapTypeId.ROADMAP,
+        zoom               : 16,
+        disableDefaultUI   : true,
+        scrollwheel        : false,
+        zoomControl        : true,
+        zoomControlOptions : {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.LEFT_TOP
+        }
+    },
+
+    map = new google.maps.Map($map[0], mapOptions),
+
+    marker = new google.maps.Marker({
+        position : new google.maps.LatLng(35.732766,51.389306),
+        map      : map,
+        title    : 'RWD Conference'
+    });
+}
